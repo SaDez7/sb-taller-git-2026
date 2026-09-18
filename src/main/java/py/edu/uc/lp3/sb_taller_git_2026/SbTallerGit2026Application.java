58a7f7e0edac6df2e3625b2312a2342d631de0fc
SaDez7 <santiago.d.baez.p@gmail.com>
2026-09-18 01:42:53 -0300

    chore: Spring Boot API REST desde start.spring.io

---
+++ src/main/java/py/edu/uc/lp3/sb_taller_git_2026/SbTallerGit2026Application.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.sb_taller_git_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTallerGit2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTallerGit2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/py/edu/uc/lp3/sb_taller_git_2026/SbTallerGit2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.sb_taller_git_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTallerGit2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
